fe3723006fdb576d91ed19f2b8856b099f95cd51
Mutter <Mutter>
2024-07-26 23:23:59 +0000

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// noinspection JSUnusedGlobalSymbols
+module.exports = {
+    env: {
+        basePath: process.env.BASE_PATH || '',
+        basePort: process.env.BASE_PORT || 3000,
+    },
+    basePath: process.env.BASE_PATH || '',
+    i18n: {
+        locales: ['en', 'ru', 'ar'],
+        defaultLocale: 'en',
+    },
+};
